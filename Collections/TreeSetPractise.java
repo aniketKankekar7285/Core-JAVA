1cec96414e090c4f45f4789e4274d9ce559afb76
aniketKankekar7285 <aniket92000@gmail.com>
2023-08-20 16:42:53 +0530

    treeset created for custom object

--- Collections/TreeSetPractise.java
+++ Collections/TreeSetPractise.java
@@ -1,5 +1,34 @@
+import java.util.Comparator;
 import java.util.List;
 import java.util.TreeSet;
+
+
+class Point implements Comparable {
+    int x;
+    int y;
+
+    Point(int a,int b){
+        x=a;
+        x=b;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Point p=(Point)o;
+        if(this.x<p.x){
+            return 1;
+        }
+        else{
+            return -1;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "x: "+x+" y: "+y;
+    }
+}
+
 
 public class TreeSetPractise
 {
@@ -12,5 +41,15 @@ public class TreeSetPractise
         ts.remove(10);
         System.out.println(ts);
         System.out.println(ts.ceiling(20));
+
+        TreeSet<Point> ts2=new TreeSet<>();
+        ts2.add(new Point(10,20));
+        ts2.add(new Point(100,20));
+        ts2.add(new Point(11,20));
+        ts2.add(new Point(10,223));
+        ts2.add(new Point(9,2));
+
+        System.out.println(ts2);
+
     }
 }
