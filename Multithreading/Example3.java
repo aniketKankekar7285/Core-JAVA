77dc98e8197272385e6a4d060c6dc78b89c30987
Aniket Kankekar <76193730+aniketKankekar7285@users.noreply.github.com>
2025-01-12 19:31:30 +0530

    added daemon thread

--- Multithreading/Example3.java
+++ Multithreading/Example3.java
@@ -3,13 +3,8 @@ class Mythread extends Thread{
     @Override
     public void run() {
        int count=1;
-       while(true){
-           try {
-               System.out.println(count++);
-               sleep(1000);
-           }catch (InterruptedException e){
-               System.out.println(e);
-           }
+       while(count<1000){
+           System.out.println(count++);
        }
     }
     Mythread(String name){
@@ -18,13 +13,15 @@ class Mythread extends Thread{
     }
 }
 public class Example3 {
-    public static void main(String args[]){
+    public static void main(String args[]) throws Exception{
         Mythread m1=new Mythread("My first thread");
         System.out.println("ID: "+m1.getId());
         System.out.println("Name: "+m1.getName());
         System.out.println("priority: "+m1.getPriority());
         System.out.println("State: "+m1.getState());
+        m1.setDaemon(true);
         m1.start();
-        m1.interrupt();
+        Thread mainThread=Thread.currentThread();
+        mainThread.join();
     }
 }
